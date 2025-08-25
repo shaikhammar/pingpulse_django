ba6b98b2d32cfa34588506a73fc17436145981ce
Ammar Shaikh <shaikhammar87@gmail.com>
2025-08-25 19:06:41 -0400

    removed register user tests

--- accounts/tests/test_api.py
+++ accounts/tests/test_api.py
@@ -1,4 +1,3 @@
-
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
@@ -8,28 +7,12 @@ User = get_user_model()
 
 class AuthAPITests(APITestCase):
 
-    def test_registration(self):
-        url = reverse("rest_register")
-        data = {
-            "email": "test@example.com",
-            "password1": "$Ay687a$",
-            "password2": "$Ay687a$"
-        }
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(User.objects.get().email, "test@example.com")
-
-    def test_login(self):
-        # First, create a user to log in with
-        email = "test@example.com"
-        password = "$Ay687a$"
-        user = User.objects.create_user(email=email, password=password)
-
+    def test_login_demouser(self):
+        # This test relies on the demouser created in the migration 0002_create_demouser.py
         url = reverse("rest_login")
         data = {
-            "email": email,
-            "password": password
+            "email": "demouser@example.com",
+            "password": "demopassword"
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
