ead86bf5923bebc64d5221872abf7ec0dab2d9cb
Ammar Shaikh <shaikhammar87@gmail.com>
2025-08-25 19:00:30 -0400

    github actions

--- accounts/migrations/0002_create_demouser.py
+++ accounts/migrations/0002_create_demouser.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 def create_demouser(apps, schema_editor):
     USER = get_user_model()
-    User.objects.create_user(email='demouser@example.com', password='demopassword')
+    USER.objects.create_user(email='demouser@example.com', password='demopassword')
 
 class Migration(migrations.Migration):
 
